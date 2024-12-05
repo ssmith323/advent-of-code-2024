4c43b706625ffe5c5d2685b009194a25ff75c0df
Stephen Smith <ssmith@Stephens-Air.attlocal.net>
2024-12-04 22:15:36 -0500

    finish day 4

--- workspaces/day04/src/__test__/part2.test.js
+++ workspaces/day04/src/__test__/part2.test.js
@@ -1,4 +1,4 @@
 const { part2 } = require('../part2.js')
 
-test('test file', () => expect(part2('./test.txt')).toBe(9))
-test('input file', () => expect(part2('./input.txt')).toBe(null))
+test('test file', () => expect(part2('test.txt')).toBe(9))
+test('input file', () => expect(part2('input.txt')).toBe(2034))
--- workspaces/day04/src/part2.js
+++ workspaces/day04/src/part2.js
@@ -5,44 +5,41 @@ const part2 = (filename) => {
   const input = getCommands(filename)
   const map = input.map((line) => line.split(''))
   let count = 0
-  for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[i].length; j++) {
-      if (
-        i >= 3 &&
-        j >= 3 &&
-        map.length - 3 > i &&
-        map[i].length - 3 > j &&
-        map[i][j] === 'X'
-      ) {
+  for (let i = 1; i < map.length - 1; i++) {
+    for (let j = 1; j < map[i].length - 1; j++) {
+      if (map[i][j] === 'A') {
         if (
           map[i - 1][j - 1] === 'M' &&
-          map[i - 2][j - 2] === 'A' &&
-          map[i - 3][j - 3] === 'S' &&
-          map[i + 1][j + 1] === 'M' &&
-          map[i + 2][j + 2] === 'A' &&
-          map[i + 3][j + 3] === 'S'
-        ) {
-          count++
-        }
-        if (
-          map[i + 1][j + 1] === 'M' &&
-          map[i + 2][j + 2] === 'A' &&
-          map[i + 3][j + 3] === 'S'
+          map[i + 1][j + 1] === 'S' &&
+          map[i - 1][j + 1] === 'M' &&
+          map[i + 1][j - 1] === 'S'
         ) {
           count++
         }
 
         if (
-          map[i + 1][j - 1] === 'M' &&
-          map[i + 2][j - 2] === 'A' &&
-          map[i + 3][j - 3] === 'S'
+          map[i - 1][j - 1] === 'M' &&
+          map[i + 1][j + 1] === 'S' &&
+          map[i - 1][j + 1] === 'S' &&
+          map[i + 1][j - 1] === 'M'
         ) {
           count++
         }
+
         if (
+          map[i - 1][j - 1] === 'S' &&
+          map[i + 1][j + 1] === 'M' &&
+          map[i - 1][j + 1] === 'S' &&
+          map[i + 1][j - 1] === 'M'
+        ) {
+          count++
+        }
+
+        if (
+          map[i - 1][j - 1] === 'S' &&
+          map[i + 1][j + 1] === 'M' &&
           map[i - 1][j + 1] === 'M' &&
-          map[i - 2][j + 2] === 'A' &&
-          map[i + -3][j + 3] === 'S'
+          map[i + 1][j - 1] === 'S'
         ) {
           count++
         }
